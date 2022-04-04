d43b313d9ade34c3014045bca557440cf8ddb8f7
Jakub Arbet <hi@jakubarbet.me>
2022-04-04 13:41:02 +0300

    Add command line interface handlers

---
+++ helpers.py
@@ -0,0 +1,10 @@
+from typing import Callable, TypeVar, Union
+
+
+R = TypeVar("R")
+def handle_cmd_with_int_argument(input: str, fn: Callable[[int], R], usage: str) -> Callable[[], Union[R, None]]:
+  if len(input.split(' ')) != 2 or not input.split(' ')[1].isdigit():
+    return lambda: print('Usage: %s', usage)
+  
+  arg = int(input.split(' ')[1])
+  return lambda: fn(arg)
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import sys
+from typing import Callable, Dict
+from helpers import handle_cmd_with_int_argument
+
+
+def list():
+  print('list')
+  pass
+
+
+def set_time_to_critical_section(time: int):
+  print('set_time_to_critical_section: %d' % time)
+  pass
+
+
+def set_time_out_interval(timeout: int):
+  print('set_time_out_interval: %d' % timeout)
+  pass
 
 
 if __name__=='__main__':
@@ -9,3 +26,26 @@ if __name__=='__main__':
 
   n = int(sys.argv[1])
   print("Launching %d processes" % n)
+
+  # Start= the command line interface
+  while True:
+    try:
+      user_input = input("$ ")
+    except EOFError as e:
+      # handle Ctrl+d as end of program
+      print()
+      sys.exit(1)
+
+    cmd = user_input.split(' ', 1)[0]
+
+    # Define the handlers for commands
+    handlers: Dict[str, Callable[[], None]] = {
+      'help': lambda: print('Supported commands: list, time-cs [t], time-p [t], help, exit'),
+      'exit': lambda: sys.exit(0),
+      'list': list,
+      'time-cs': handle_cmd_with_int_argument(user_input, set_time_to_critical_section, 'time-cs [t]'),
+      'time-p': handle_cmd_with_int_argument(user_input, set_time_out_interval, 'time-p [t]'),
+    }
+
+    # Execute appropriate handler or print error message
+    handlers.get(cmd, lambda: len(cmd) > 0 and not cmd.isspace() and print('%s: command not found' % cmd))()
